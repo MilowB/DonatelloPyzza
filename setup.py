c56b0bfe284ef81924eca99e5b65c4770b521be9
Mickael <mickael.bettinelli@protonmail.com>
2023-04-11 18:38:43 +0200

    setup file

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+# this grabs the requirements from requirements.txt
+REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
+
+setup(
+    install_requires=REQUIREMENTS
+)
